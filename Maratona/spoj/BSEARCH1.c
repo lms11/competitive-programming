3036794964ba1db6187f38622cbc1f15a748f01b
Lucas Moreira <lucas.moreira@me.com>
2015-04-23 16:54:43 -0300

    Update

--- Maratona/spoj/BSEARCH1.c
+++ Maratona/spoj/BSEARCH1.c
@@ -1,54 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct pair {
-	long long int value, position;
-} Pair;
-
-int compare(const void * a, const void * b) {
-	long long int val_a = ((const Pair *)a)->value;
-	long long int val_b = ((const Pair *)b)->value;
-	return (val_a < val_b) ? -1 : (val_a > val_b);
-}
-
-int bSearchCompare(const void * a, const void * b) {
-	// a => key, b => element
-	long long int val_a = *(long long int *)a;
-	long long int val_b = ((const Pair *)b)->value;
-
-	return (val_a < val_b) ? -1 : (val_a > val_b) ? 1 : 0;
+long long int binarySearch(long long int vet[], long long int value, long long int size)
+{
+     long long int min = 0;
+     long long int max = size - 1;
+     long long int mid;
+     while (min <= max) 
+     {
+          mid = (min + max)/2;
+          if (value == vet[mid])
+               return mid;
+          else if (value < vet[mid])
+               max = mid-1;
+          else
+               min = mid+1;
+     }
+     return -1;
 }
 
 int main() {
 	long long int n, q, i, tmp;
-	Pair * object;
 
 	scanf("%lld %lld", &n, &q);
 
-	Pair values[100000];
-	long long int queries[500000];
+	long long int values[n], queries[q];
 
 	for (i = 0; i < n; i++) {
-		scanf("%lld", &tmp);
-		if (tmp > 1000000000 || tmp < -1000000000) return 0;
-
-		values[i].value = tmp;
-		values[i].position = i; 
+		scanf("%lld", &values[i]);
 	}
-
-	qsort(values, n, sizeof(Pair), compare);
 
 	for (i = 0; i < q; i++) {
 		scanf("%lld", &queries[i]);
-		if (queries[i] > 1000000000 || queries[i] < -1000000000) return 0;
 	}
 
 	for (i = 0; i < q; i++) {
-		// looking for queries[z]
-		object = (Pair *) bsearch(&queries[i], values, n, sizeof(Pair), bSearchCompare);
-
-		if (object == NULL) printf("-1\n");
-		else printf("%lld\n", object->position);
+		printf("%lld\n", binarySearch(values, queries[i], n));
 	}
 
 	return 0;
