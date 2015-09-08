2c5ec0b919ebe6cf9268c9965836a4a3c14e6951
Lucas Moreira <lucas.moreira@me.com>
2015-09-07 23:20:43 -0300

    Update

--- Exercicios/spojbr/rmapa11.cpp
+++ Exercicios/spojbr/rmapa11.cpp
@@ -1,68 +1,108 @@
 #include <cstdio>
 #include <queue>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-#define NODES 700
+#define MAX 700
+vector<int> graph[MAX];
+bool visited[MAX];
+int weight[MAX][MAX];
 
-vector< pair<int, int> > graph[NODES];
-bool visited[NODES];
-queue<int> q;
+class MinPQ {
+	public:
+		pair<int, int> popMin();
+		void insert(int a, int b);
+		bool empty();
+		void print();
 
-long long int mst(int n) {
-	long long int sum = 0;
-	int min;
+	private:
+		int indexOfInsertion(int weight, int a, int b);
+		vector<pair<int, int> > v;
+};
 
-	for (int x = 0; x < NODES; x++)
-		visited[x] = false;
-
-	q.push(n);
-
-	while(!q.empty()) {
-		int node = q.front();
-		q.pop();
-
-		if (visited[node]) continue;
-
-		visited[node] = true;
-		min = -1;
-
-		for (vector< pair<int, int> >::iterator it = graph[node].begin(); it != graph[node].end(); it++) {
-			if (!visited[it->first]) {
-				q.push(it->first);
-
-				if (it->second < min || min == -1) {
-					min = it->second;
-				}
-			}
-		}
-
-		if (min >= 0) sum += min;
-	}
-
-	return sum;
+bool MinPQ::empty() {
+	return this->v.size() == 0;
 }
 
-int main() {
-	int a, b, c, d, e;
+pair<int, int> MinPQ::popMin() {
+	pair<int, int> p = this->v[0];
+	this->v.erase(v.begin());
+	return p;
+}
 
-	scanf("%d %d", &a, &b);
+static bool compare(const pair<int, int> a, const pair<int, int> b) {
+	return weight[a.first][a.second] < weight[b.first][b.second];
+}
 
-	while(b--) {
-		scanf("%d %d %d", &c, &d, &e);
+void MinPQ::insert(int a, int b) {
+	this->v.insert(std::lower_bound(this->v.begin(), this->v.end(), make_pair(a, b), compare), make_pair(a, b));
+}
+
+void MinPQ::print() {
+	vector<pair<int, int> >::iterator it;
+	printf("MinPQ vector:\n");
+	for (it = this->v.begin(); it != this->v.end(); it++)
+		printf("  %d-%d  %d\n", it->first, it->second, weight[it->first][it->second]);
+
+	printf("\n\n");
+}
+
+void visit(MinPQ *pq, int x) {
+	if (visited[x] == false) {
+		visited[x] = true;
+		vector<int>::iterator it;
+
+		for (it = graph[x].begin(); it != graph[x].end(); it++) {
+			if (visited[*it] == false)
+				pq->insert(*it, x);
+		}
+	}
+}
+
+int mst() {
+	MinPQ *pq = new MinPQ();
+	vector<pair<int, int> > mst;
+
+	visit(pq, 1);
 	
-		graph[c].push_back(make_pair(d, e));
-		graph[d].push_back(make_pair(c, e));
+	while(!pq->empty()) {
+		pair<int, int> e = pq->popMin();
+		int v = e.first, w = e.second;
+		if (visited[v] && visited[w]) continue;
+		mst.push_back(e);
+		if (!visited[v]) visit(pq, v);
+		if (!visited[w]) visit(pq, w); 
 	}
 
-	long long int sum = 0;
-
-	for (int x = 1; x <= a; x++) {
-		if (!visited[x])
-			sum += mst(x);
+	int res = 0;
+	vector<pair<int, int> >::iterator it;
+	for (it = mst.begin(); it != mst.end(); it++) {
+		res += weight[it->first][it->second];
 	}
 
-	printf("%lld\n", sum);
+	return res;
+}
+
+
+
+int main() {
+	int n, m;
+	int a, b, c;
+
+	scanf("%d %d", &n, &m);
+
+	while(m--) {
+		scanf("%d %d %d", &a, &b, &c);
+		weight[a][b] = weight[b][a] = c;
+		visited[a] = visited[b] = false;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+
+	printf("%d\n", mst());
 
 	return 0;
 }
+
+
